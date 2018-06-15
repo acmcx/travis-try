f83b1e4419f3719f2d4cbb7fd3c72447b8165bc9
antik9 <illarionovanton@gmail.com>
2018-06-15 13:45:04 +0300

    Auto rebuild

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 def hello():
     """
     Returns Hello, World!
     """
     return "Hello, World!"
+
+def goodbye():
+    return 2
 
 if __name__ == "__main__":
     print(hello())
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
-from hello import hello
+from hello import hello, goodbye
 
 class TestHello(unittest.TestCase):
 
     def test_hello(self):
         self.assertEqual(hello(), "Hello, World!")
+
+    def test_goodbye(self):
+        self.assertEqual(goodbye(), 2)
 
 
 if __name__ == "__main__":
